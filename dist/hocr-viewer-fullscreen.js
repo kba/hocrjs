f66d1647a2e03a06703e4a5abbf8a7c0e5e293f3
Konstantin Baierer <unixprog@gmail.com>
2016-10-04 03:59:50 +0200

    x

--- dist/hocr-viewer-fullscreen.js
+++ dist/hocr-viewer-fullscreen.js
@@ -21,11 +21,13 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-window.hocrViewerInit = function hocrViewerInit() {
-    window.hocrViewer = new window.HocrViewer({root: document.querySelector('body')});
-    window.hocrViewer.init();
-}
-window.addEventListener('load', window.hocrViewerInit);
+(function() {
+    window.hocrViewerInit = function hocrViewerInit() {
+        window.hocrViewer = new window.HocrViewer({root: document.querySelector('body')});
+        window.hocrViewer.init();
+    }
+    window.addEventListener('load', window.hocrViewerInit);
+}());
 (function(global)  {
     function HocrParser() {}
 
--- js/fullscreen-init.js
+++ js/fullscreen-init.js
@@ -1,5 +1,7 @@
-window.hocrViewerInit = function hocrViewerInit() {
-    window.hocrViewer = new window.HocrViewer({root: document.querySelector('body')});
-    window.hocrViewer.init();
-}
-window.addEventListener('load', window.hocrViewerInit);
+(function() {
+    window.hocrViewerInit = function hocrViewerInit() {
+        window.hocrViewer = new window.HocrViewer({root: document.querySelector('body')});
+        window.hocrViewer.init();
+    }
+    window.addEventListener('load', window.hocrViewerInit);
+}());
