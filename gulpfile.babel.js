cfecc93c6c4d6e7fc2eb71e6c81b34279f46d0f9
Konstantin Baierer <unixprog@gmail.com>
2016-11-08 11:17:13 +0100

    Add userjs gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,17 +6,19 @@ import webpackConfig from './webpack.config.babel';
 import {exec} from 'child_process';
 import tape from 'gulp-tape';
 import tapColorize from 'tap-colorize';
+import replace from 'gulp-replace';
 
 const paths = {
-  allSrcJs: 'js/**/*.jsx?',
-  testSrcJs: 'src/test/*.js',
-  sharedSrcJs: 'src/shared/**/*.js',
-  clientEntryPoint: 'src/shared/parser.js',
-  gulpFile: 'gulpfile.babel.js',
-  webpackFile: 'webpack.config.babel.js',
-  distDir: 'dist',
-  testDir: 'test',
-  libDir: 'lib',
+    allSrcJs: 'js/**/*.jsx?',
+    testSrcJs: 'src/test/*.js',
+    sharedSrcJs: 'src/shared/**/*.js',
+    clientEntryPoint: 'src/browser/viewer.js',
+    gulpFile: 'gulpfile.babel.js',
+    webpackFile: 'webpack.config.babel.js',
+    userjs: 'src/userjs/hocr-viewer.user.js',
+    distDir: 'dist',
+    testDir: 'test',
+    libDir: 'lib',
 };
 
 gulp.task('clean', () => {
@@ -42,7 +44,19 @@ gulp.task('build', ['clean'], () => {
     .pipe(gulp.dest(paths.libDir));
 });
 
-gulp.task('main', ['build'], (callback) => {
+gulp.task('userjs', [], () => {
+  var ASSET_SERVER = (process.env.ASSET_SERVER || 'https://kba.github.io/hocrjs/dist');
+  var UPDATE_SERVER = (process.env.UPDATE_SERVER || ASSET_SERVER);
+  gulp.src(paths.userjs)
+    .pipe(babel())
+    .pipe(replace('__UPDATE_SERVER__', UPDATE_SERVER))
+    .pipe(replace('__ASSET_SERVER__', ASSET_SERVER))
+    .pipe(replace('__DATE__', new Date().getTime() / 1000))
+    .pipe(replace(/^.use strict.;\n\n/, ''))
+    .pipe(gulp.dest(paths.distDir));
+});
+
+gulp.task('browser', ['build'], (callback) => {
   gulp.src(paths.clientEntryPoint)
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest(paths.distDir));
@@ -52,4 +66,4 @@ gulp.task('watch', () => {
   gulp.watch(paths.sharedSrcJs, ['build']);
 });
 
-gulp.task('default', ['watch', 'main']);
+gulp.task('default', ['watch', 'browser']);
