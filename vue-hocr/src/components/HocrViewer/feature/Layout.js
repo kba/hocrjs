119c5ec644b287afe981c2c1973895bbcfd12a85
Stefan Weil <sw@weilnetz.de>
2018-03-25 13:43:39 +0200

    Fix calculation of width and height from box coordinates
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- vue-hocr/src/components/HocrViewer/feature/Layout.js
+++ vue-hocr/src/components/HocrViewer/feature/Layout.js
@@ -10,8 +10,8 @@ export default class Layout {
       el.style.position = 'fixed'
       el.style.left    = bbox[0] + "px"
       el.style.top     = bbox[1] + "px"
-      el.style.width   = bbox[2] - bbox[0] + "px"
-      el.style.height  = bbox[3] - bbox[1] + "px"
+      el.style.width   = bbox[2] - bbox[0] + 1 + "px"
+      el.style.height  = bbox[3] - bbox[1] + 1 + "px"
     })
   }
 
