0374ba2fd295188cdf517284dd898a61f5034dff
Stefan Weil <sw@weilnetz.de>
2018-03-29 23:02:34 +0200

    Update font scaling
    - Removed unused maxFontSize.
    - wrap is now a class member (avoids parameter passing).
    - Fix size calculation for narrow boxes (like single "l" or "I").
    - Remove cache as it results in wrong size for same text in boxes of
      different size.
    - Remove final decrement of font size.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- vue-hocr/src/components/HocrViewer/feature/ScaleFont.js
+++ vue-hocr/src/components/HocrViewer/feature/ScaleFont.js
@@ -4,53 +4,46 @@ export default class ScaleFont {
 
   constructor({fontFamily='x', fonts={}}) {
     this.fontFamily = fontFamily
-    this.maxFontSize = 128
     this.minFontSize = 2
     this.wrapClass   = 'hocr-viewer-wrap'
     this.cache = {}
+    // wrapper element containing wrappers for font-size expansion
+    this.wrap = document.createElement('span')
+    this.wrap.classList.add(this.wrapClass)
   }
 
   apply(dom) {
     console.time('toggleScaleFont')
 
-    // wrapper element containing wrappers for font-size expansion
-    const wrap = document.createElement('span')
-    wrap.classList.add(this.wrapClass)
-    document.body.appendChild(wrap)
-    HocrDOM.queryHocrAll(dom, {terminal: true}).forEach((el) => this.scaleFont(el, wrap))
-    wrap.remove()
+    document.body.appendChild(this.wrap)
+    HocrDOM.queryHocrAll(dom, {terminal: true}).forEach((el) => this.scaleFont(el))
 
     console.timeEnd('toggleScaleFont')
   }
 
-  scaleFont(el, wrap) {
+  scaleFont(el) {
     const text = el.textContent.trim()
     if (text.length === 0) return
-    if (!(text in this.cache)) {
-      // wrap.setAttribute('class', el.getAttribute('class'))
-      // wrap.style.width = '100%'
-      wrap.style.fontFamily = el.style.fontFamily
-      wrap.innerHTML = text
-      const height = parseInt(el.style.height.replace('px', ''))
-      const width = parseInt(el.style.width.replace('px', ''))
-      let fontSize = Math.min(width, height)
-      if (fontSize <= this.minFontSize) {
-        this.cache[text] = this.minFontSize
-      } else {
-        // console.log({fontSize, width: width, height:height, min, el})
-        wrap.style.fontSize = fontSize + 'px'
-        if (fontSize > this.minFontSize && wrap.offsetHeight > height) {
-          fontSize -= wrap.offsetHeight - height
-          wrap.style.fontSize = fontSize + 'px'
-        }
-        while (fontSize > this.minFontSize && wrap.offsetWidth > width) {
-          wrap.style.fontSize = --fontSize + 'px'
-        }
-        // if (iterations > 1) console.debug(iterations, text, wrap.offsetHeight, height, wrap.offsetWidth, width)
-        this.cache[text] = fontSize - 1
+    this.wrap.style.fontFamily = el.style.fontFamily
+    this.wrap.innerHTML = text
+    const height = parseInt(el.style.height.replace('px', ''))
+    const width = parseInt(el.style.width.replace('px', ''))
+    let fontSize = height
+    if (fontSize <= this.minFontSize) {
+      fontSize = this.minFontSize
+    } else {
+      // console.log({fontSize, width: width, height:height, min, el})
+      this.wrap.style.fontSize = fontSize + 'px'
+      if (fontSize > this.minFontSize && this.wrap.offsetHeight > height) {
+        fontSize -= this.wrap.offsetHeight - height
+        this.wrap.style.fontSize = fontSize + 'px'
       }
+      while (fontSize > this.minFontSize && this.wrap.offsetWidth > width) {
+        this.wrap.style.fontSize = --fontSize + 'px'
+      }
+      // if (iterations > 1) console.debug(iterations, text, this.wrap.offsetHeight, height, this.wrap.offsetWidth, width)
     }
-    el.style.fontSize = this.cache[text] + 'px'
+    el.style.fontSize = fontSize + 'px'
   }
 
 }
