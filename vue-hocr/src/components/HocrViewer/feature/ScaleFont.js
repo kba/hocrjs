dccd8566779822a07b5b8ca648ccc1e64d4fb08b
Stefan Weil <sw@weilnetz.de>
2018-03-31 15:07:58 +0200

    More improvements for font scaling
    - Create and remove the wrap element in apply() as in the original code.
    - Directly calculate the font size from first try.
    
    The direct calculation of the font size fixes too small font sizes with
    text which neither has ascenders nor descenders. It also further improves
    the speed.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- vue-hocr/src/components/HocrViewer/feature/ScaleFont.js
+++ vue-hocr/src/components/HocrViewer/feature/ScaleFont.js
@@ -6,17 +6,18 @@ export default class ScaleFont {
     this.fontFamily = fontFamily
     this.minFontSize = 2
     this.wrapClass   = 'hocr-viewer-wrap'
-    this.cache = {}
-    // wrapper element containing wrappers for font-size expansion
-    this.wrap = document.createElement('span')
-    this.wrap.classList.add(this.wrapClass)
+    this.wrap = {}
   }
 
   apply(dom) {
     console.time('toggleScaleFont')
 
+    // wrapper element containing wrappers for font-size expansion
+    this.wrap = document.createElement('span')
+    this.wrap.classList.add(this.wrapClass)
     document.body.appendChild(this.wrap)
     HocrDOM.queryHocrAll(dom, {terminal: true}).forEach((el) => this.scaleFont(el))
+    this.wrap.remove()
 
     console.timeEnd('toggleScaleFont')
   }
@@ -29,19 +30,14 @@ export default class ScaleFont {
     const height = parseInt(el.style.height.replace('px', ''))
     const width = parseInt(el.style.width.replace('px', ''))
     let fontSize = height
-    if (fontSize <= this.minFontSize) {
-      fontSize = this.minFontSize
-    } else {
-      // console.log({fontSize, width: width, height:height, min, el})
+    if (fontSize > this.minFontSize) {
       this.wrap.style.fontSize = fontSize + 'px'
-      if (fontSize > this.minFontSize && this.wrap.offsetHeight > height) {
-        fontSize -= this.wrap.offsetHeight - height
-        this.wrap.style.fontSize = fontSize + 'px'
-      }
-      while (fontSize > this.minFontSize && this.wrap.offsetWidth > width) {
-        this.wrap.style.fontSize = --fontSize + 'px'
-      }
-      // if (iterations > 1) console.debug(iterations, text, this.wrap.offsetHeight, height, this.wrap.offsetWidth, width)
+      const fontSizeH = fontSize * height / this.wrap.offsetHeight
+      const fontSizeW = fontSize * width / this.wrap.offsetWidth
+      fontSize = (fontSizeH < fontSizeW) ? fontSizeH : fontSizeW
+    }
+    if (fontSize < this.minFontSize) {
+      fontSize = this.minFontSize
     }
     el.style.fontSize = fontSize + 'px'
   }
